ae553f283db149972c9d87b73d94cf6a0b4efe16
Aleksis Toren <aleksistoren@gmail.com>
2023-07-05 04:15:17 +0200

    Refactoring of logs.

--- parser/PcapParser.h
+++ parser/PcapParser.h
@@ -20,7 +20,7 @@ class PcapParser {
     std::ofstream logFile;
 
 public:
-    PcapParser(const std::string& path, const std::string &logPath = "logsPcapData.txt")
+    PcapParser(const std::string &path, const std::string &logPath = "logsPcapData.txt")
             : file(path, std::ios::binary), logFile(logPath) {
         if (!file) {
             throw std::runtime_error("Unable to open pcap file: " + path);
@@ -30,12 +30,10 @@ public:
         file.read(reinterpret_cast<char *>(&globalHeader), sizeof(globalHeader));
 
         logFile << "Global Header:\n"
-                << "Magic Number: " << std::hex << globalHeader.magicNumber << "\n"
-                << "Version: " << globalHeader.versionMajor << "." << globalHeader.versionMinor << "\n"
-                << "Time Zone Correction: " << globalHeader.thiszone << "\n"
-                << "Timestamp Accuracy: " << globalHeader.sigfigs << "\n"
-                << "Max Length of Packet Capture: " << globalHeader.snaplen << "\n"
-                << "Network: " << globalHeader.network << "\n\n";
+                << globalHeader
+                << std::endl
+                << std::endl;
+
     }
 
     void parse() {
@@ -48,7 +46,7 @@ public:
 
             file.read(reinterpret_cast<char *>(&packet.packetHeader), sizeof(packet.packetHeader));
 
-            if (packet.packetHeader.inclLen == 0 && file.eof()){
+            if (packet.packetHeader.inclLen == 0 && file.eof()) {
                 break;
             }
 
@@ -58,46 +56,41 @@ public:
 
             file.read(reinterpret_cast<char *>(&packet.udpHeader), sizeof(packet.udpHeader));
 
-            // print packet header info...
-
-            // print UDP header info...
-            logFile << "UDP Header:\n"
-                    << "Source Port: " << packet.udpHeader.srcPort << "\n"
-                    << "Destination Port: " << packet.udpHeader.destPort << "\n"
-                    << "Length: " << packet.udpHeader.length << "\n"
-                    << "Checksum: " << packet.udpHeader.checksum << "\n";
-
             // Reading packet data
             packet.packetData.resize(
                     packet.packetHeader.inclLen - sizeof(EthernetHeader) - sizeof(IpHeader) - sizeof(UdpHeader));
             file.read(reinterpret_cast<char *>(packet.packetData.data()),
                       packet.packetHeader.inclLen - sizeof(EthernetHeader) - sizeof(IpHeader) - sizeof(UdpHeader));
-            //parsedResult.push_back(packet);
-            // Print the packet data
-            logFile << "Packet data: ";
-            for (char c : packet.packetData) {
-                logFile << std::hex << std::setw(2) << std::setfill('0')
-                        << static_cast<int>(static_cast<unsigned char>(c)) << ' ';
-            }
-            logFile << "\n" << "\n";
+
+            logFile << "Packet: \n"
+                    << packet
+                    << std::endl;
 
             IncrementalPacket incrementalPacket;
-            if (!Decoder::tryDecodeIncremental(packet.packetData, incrementalPacket)){
+            if (!Decoder::tryDecodeIncremental(packet.packetData, incrementalPacket)) {
                 SnapshotPacket snapshotPacket;
-                if (!Decoder::tryDecodeSnapshot(packet.packetData, snapshotPacket)){
+                if (!Decoder::tryDecodeSnapshot(packet.packetData, snapshotPacket)) {
                     ++cntBadPackets;
-                    logFile<<"Incorrect message"<<std::endl;
-                }else{
-                    logFile<<snapshotPacket<<std::endl;
+                    logFile << "Incorrect message"
+                            << std::endl
+                            << std::endl;
+                } else {
+                    logFile << snapshotPacket
+                            << std::endl
+                            << std::endl;
                 }
-            }else{
-                logFile<<incrementalPacket<<std::endl;
+            } else {
+                logFile << incrementalPacket
+                        << std::endl
+                        << std::endl;
             }
 
             index++;
         }
 
-        logFile<<"cntBadPackets = "<<cntBadPackets<<"/"<<index<<"\n";
-        std::cout<<"cntBadPackets = "<<cntBadPackets<<"/"<<index<<"\n";
+        logFile << "cntBadPackets = " << cntBadPackets << "/" << index
+                << std::endl;
+        std::cout << "cntBadPackets = " << cntBadPackets << "/" << index
+                  << std::endl;
     }
 };

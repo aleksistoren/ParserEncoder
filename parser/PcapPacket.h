4ad971e3ad9b625a08b426ff13143866d0514b04
Aleksis Toren <aleksistoren@gmail.com>
2023-07-05 04:04:14 +0200

    Added operator << to PcapPacket.

--- parser/PcapPacket.h
+++ parser/PcapPacket.h
@@ -14,3 +14,15 @@ struct PcapPacket{
     UdpHeader udpHeader;
     std::vector<uint8_t> packetData;
 };
+
+std::ostream& operator<<(std::ostream& os, const PcapPacket& packet) {
+    os << "Packet Header: " << packet.packetHeader << std::endl;
+    os << "Ethernet Header: " << packet.ethernetHeader << std::endl;
+    os << "IP Header: " << packet.ipHeader << std::endl;
+    os << "UDP Header: " << packet.udpHeader << std::endl;
+    os << "Packet Data: ";
+    for (const auto& byte : packet.packetData) {
+        os << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte) << " ";
+    }
+    return os;
+}
